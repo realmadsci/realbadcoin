d4535a72e79f34b6c1a9358e850d48a7a19fe413
realmadsci <6604845-realmadsci@users.noreply.gitlab.com>
2022-03-26 11:33:28 -0400

    Make peers list more permanent by moving it to localStorage

--- src/ConnectionManager.js
+++ src/ConnectionManager.js
@@ -70,14 +70,14 @@ export class ConnectionManager {
         else if (remove) {
             // If the user specifically deleted the connection, then wipe it from the restart history:
             this.peerHistory = this.peerHistory.filter((e, i) => e !== peer);
-            sessionStorage.setItem("peer_history", JSON.stringify(this.peerHistory));
+            localStorage.setItem("peer_history", JSON.stringify(this.peerHistory));
         }
         if (newState === "connected") {
             // Keep track of previously connected peers so we can try to dial them up again next time!
             this.peerHistory.push(peer);
             // Only keep unique ones!
             this.peerHistory = this.peerHistory.filter((e, i) => this.peerHistory.indexOf(e) === i);
-            sessionStorage.setItem("peer_history", JSON.stringify(this.peerHistory));
+            localStorage.setItem("peer_history", JSON.stringify(this.peerHistory));
 
             this._updateNotifier.emit('new_peer', peer);
         }
@@ -96,7 +96,7 @@ export class ConnectionManager {
         //https://anseki.github.io/gnirts/
         const a = (388).toString(36).toLowerCase() + (function () { var Z = Array.prototype.slice.call(arguments), H = Z.shift(); return Z.reverse().map(function (Q, P) { return String.fromCharCode(Q - H - 7 - P) }).join('') })(29, 140, 147, 144, 144) + (28210).toString(36).toLowerCase() + (1203767).toString(36).toLowerCase().split('').map(function (r) { return String.fromCharCode(r.charCodeAt() + (-39)) }).join('') + (596).toString(36).toLowerCase() + (function () { var o = Array.prototype.slice.call(arguments), i = o.shift(); return o.reverse().map(function (L, H) { return String.fromCharCode(L - i - 38 - H) }).join('') })(0, 101, 104, 101, 99, 97, 151, 149, 151, 151, 157, 154, 147, 147) + (7482579).toString(36).toLowerCase() + (function () { var L = Array.prototype.slice.call(arguments), w = L.shift(); return L.reverse().map(function (c, N) { return String.fromCharCode(c - w - 39 - N) }).join('') })(4, 153, 146);
         this.myId = sessionStorage.getItem("peer_id") || generateSlug(2).replace("-", "_");
-        this.peerHistory = JSON.parse(sessionStorage.getItem("peer_history") || "[]");
+        this.peerHistory = JSON.parse(localStorage.getItem("peer_history") ?? sessionStorage.getItem("peer_history") ?? "[]");
         this.server = new Peer(this.myId, {
             host: 'coinpeers.realmadsci.com',
             port: 8080,
